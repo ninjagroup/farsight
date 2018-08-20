be0f983291e150d36e652b3e0ea02a1082bca11b
NinjaKimOne <sksmsrla0101@gmail.com>
2018-08-20 04:27:00 +0300

    fs6818 part

--- fs6818.c
+++ fs6818.c
@@ -1,29 +1,47 @@
 #include "fs6818.h"
 
+void init_led()
+{
+	//init red 
+	LED_RED &= ~(3<<24);
+	LED_RED_ENB |= (1<<28);
+	LED_RED_OUT &= ~(1<<28);
+	//init green 
+	LED_GREEN &= ~(3<<26);
+	LED_GREEN_ENB |= (1<<13);
+	LED_GREEN_OUT &= ~(1<<13);
+	//init blue 
+	LED_BLUE &= ~(3<<24);
+	LED_BLUE |= (2<<24);
+	LED_BLUE_ENB |= (1<<12);
+	LED_BLUE_OUT &= ~(1<<12);
+}
 
 void open_led(int color)
 {
 	if(color == 1)
 	{
-
+		LED_RED_OUT |= (1<<28);
 	}
 	else if(color == 2)
 	{
-
+		LED_GREEN_OUT |= (1<<13);
 	}
 	else{
-
+		LED_BLUE_OUT |= (1<<12);
 	}
 }
 
 void close_led()
 {
-
+	LED_RED_OUT &= ~(1<<28);
+	LED_GREEN_OUT &= ~(1<<13);
+	LED_BLUE_OUT &= ~(1<12);
 }
 
 void open_pwm()
 {
-
+	
 }
 
 void close_pwm()
--- fs6818.h
+++ fs6818.h
@@ -3,6 +3,20 @@
 #define __FS6818_H__
 #include "header.h"
 
+#define LED_RED  
+#define LED_BLUE 
+#define LED_GREEN 
+
+#define LED_RED_ENB
+#define LED_GREEN_ENB
+#define LED_BLUE_ENB 
+
+#define LED_RED_OUT 
+#define LED_GREEN_OUT 
+#define LED_BLUE_OUT 
+
+
+int initFlag = 0;
 
 //int 1 red 2 blue 3 green
 void open_led(int color);
